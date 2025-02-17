788c81d9a483b97d321c393c2402ac2ad23f6934
Arsalan Akhter <59329176+TheArslan23@users.noreply.github.com>
2025-02-17 17:32:51 +0530

    Add files via upload

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { 
+  convexAuthNextjsMiddleware, 
+  createRouteMatcher, 
+  isAuthenticatedNextjs,
+  nextjsMiddlewareRedirect
+} from "@convex-dev/auth/nextjs/server";
+
+const isPublicPage = createRouteMatcher(["/auth"]);
+
+export default convexAuthNextjsMiddleware(async (request) => {
+  const isAuthenticated = await isAuthenticatedNextjs(); // Removed `request`
+
+  if (!isPublicPage(request) && !isAuthenticated) {
+    return nextjsMiddlewareRedirect(request, "/auth");
+  }
+
+  if (isPublicPage(request) && isAuthenticated) {
+    return nextjsMiddlewareRedirect(request, "/");
+  }
+});
+
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
