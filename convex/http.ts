cc81128e29467871c2299c2b1a22724bdccc36e5
Arsalan Akhter <59329176+TheArslan23@users.noreply.github.com>
2025-02-18 10:32:41 +0530

    Update http.ts

--- convex/http.ts
+++ convex/http.ts
@@ -3,21 +3,6 @@ import { auth } from "./auth";
 
 const http = httpRouter();
 
-// Middleware to add CORS headers
-http.use(async (req, res, next) => {
-  res.headers.set("Access-Control-Allow-Origin", "chrome-extension://YOUR_EXTENSION_ID");
-  res.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.headers.set("Access-Control-Allow-Headers", "Authorization, Content-Type");
-
-  if (req.method === "OPTIONS") {
-    res.status(204).end();
-    return;
-  }
-
-  await next();
-});
-
-// Add authentication routes
 auth.addHttpRoutes(http);
 
 export default http;
